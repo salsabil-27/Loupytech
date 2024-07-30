6565a40a97ab39fd7ff1cc2c7afda01aaa7e447a
Zaabar Salsabil <zaabarsalsabil@gmail.com>
2024-07-30 16:02:33 +0100

    salsabil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'login', component: LoginComponent },
+
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+
+
+];
